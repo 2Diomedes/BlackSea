c2c6d572933dad04313ce849b48daabc627152e0
2Diomedes <t.martinteyssier@gmail.com>
2018-09-25 14:02:37 +0200

    first commit

---
+++ footer.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package _s
+ */
+
+?>
+
+
+<footer>
+	<ul>
+		<li>List part</li>
+		<li>List part</li>
+		<li>List part</li>
+		<li>List part</li>
+	</ul>
+</footer>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package _s
+ */
+
+get_header();
+?>
+<main>
+	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci et itaque inventore odit ad! Alias totam laborum numquam in assumenda architecto nobis ducimus, asperiores, quas quos quia! Eum, possimus mollitia.
+</main>
+
+<?php
+get_sidebar();
+get_footer();
